939ed3113a9d02f9098224888a1174635143475a
Jaber Al Nahian <rijans.x@gmail.com>
2019-02-03 17:00:21 +0600

    mcr created with supplier table

---
+++ app/Http/Controllers/SuppilerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Suppiler;
+use Illuminate\Http\Request;
+
+class SuppilerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Suppiler  $suppiler
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Suppiler $suppiler)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Suppiler  $suppiler
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Suppiler $suppiler)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Suppiler  $suppiler
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Suppiler $suppiler)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Suppiler  $suppiler
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Suppiler $suppiler)
+    {
+        //
+    }
+}
