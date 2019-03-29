1ddc5c6fe4caea6f826b3f813e66d54c96d1bebb
Jaber Al Nahian <rijans.x@gmail.com>
2019-03-29 23:01:35 +0600

    Added Users and Roles

--- resources/views/panel/includes/menu.blade.php
+++ resources/views/panel/includes/menu.blade.php
@@ -80,6 +80,23 @@
                     <li class="nav-item"><a class="nav-link" href="{{url('/brands/')}}">All Brands</a></li>
                 </ul>
             </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                         class="feather feather-layers">
+                        <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                        <polyline points="2 17 12 22 22 17"></polyline>
+                        <polyline points="2 12 12 17 22 12"></polyline>
+                    </svg>
+                    Users & Roles
+                    <i class="fa fa-caret-down pull-right"></i>
+                </a>
+                <ul class="nav subnav">
+                    <li class="nav-item"><a class="nav-link" href="{{url('/user/new/')}}">Add New</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{url('/users/')}}">All Users</a></li>
+                </ul>
+            </li>
             {{--<li class="nav-item dropdown">--}}
                 {{--<a class="nav-link dropdown-toggle" href="#">--}}
                     {{--<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>--}}
